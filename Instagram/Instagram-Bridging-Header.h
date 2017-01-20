231671c6071b608f644c341063e5f24999e87f05
xuehua <xuehua@gmail.com>
2017-01-19 22:20:54 -0800

    Added AWS frameworks to be used and added Instagram-Bridging-Header.h

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by Xuehua Chen on 1/19/17.
+//  Copyright © 2017 Xuehua Chen. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AWSCore/AWSCore.h>
+#import <AWSCognito/AWSCognito.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSS3/AWSS3.h>
+
+#endif /* Instagram_Bridging_Header_h */
